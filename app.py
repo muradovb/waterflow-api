965db5c605180ac8e2330364b9c49adcc638e5b7
Bayram Muradov <bayrammuradovm@gmail.com>
2019-08-07 21:48:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,14 @@ colors = [
           "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
           "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
 
-
+#endpoint to receive data 
 @app.route('/send-data') #GET requests will be blocked
 def populateData():
     data = request.args.get('data')
     values.append(data)
     return '''<h1>The received value is: {}</h1>'''.format(data)
 
+#root
 @app.route('/') #GET requests will be blocked
 def returnRoot():
     return '''<h1>root</h1>'''
@@ -35,6 +36,9 @@ def line():
     line_values=values
     return render_template('line_chart.html', title='WaterFlow Graph', max=17000, labels=line_labels, values=line_values)
 
+@app.route('/show-data') #GET requests will be blocked
+def showData():
+    return '''<h1>The received values are: {}</h1>'''.format(values)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
