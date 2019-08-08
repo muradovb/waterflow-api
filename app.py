1e942c1f93939d1d084376af9504b8c4f6fe6b80
Bayram Muradov <bayrammuradovm@gmail.com>
2019-08-08 10:27:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,10 @@ colors = [
 #endpoint to receive data 
 @app.route('/send-data') #GET requests will be blocked
 def populateData():
-    data = request.args.get('data')
-    if data<0
+    data = int(request.args.get('data'))
+    if(data<0):
          values.clear()
-    else
+    else:
          values.append(data)
     return '''<h1>The received value is: {}</h1>'''.format(data)
 
@@ -39,16 +39,17 @@ def returnRoot():
 def line():
     line_labels=labels
     line_values=values
-    return render_template('line_chart.html', title='WaterFlow Graph', max=17000, labels=line_labels, values=line_values)
+    return render_template('line_chart.html', title='WaterFlow Graph', max=1500, labels=line_labels, values=line_values)
 
 @app.route('/show-data') #GET requests will be blocked
 def showData():
     return '''<h1>The received values are: {}</h1>'''.format(values)
 
-@app.route('/show-data-json') #GET requests will be blocked
-def showData():
+@app.route('/data-json') #GET requests will be blocked
+def showDataJson():
     return jsonify(values)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+
